789b3bbbf917672a9b8a1c8e9afb7fb77783a13b
Parker <parkcity.pf@gmail.com>
2017-12-12 21:25:35 -0800

    Two contracts from and to for ether transfer

---
+++ EtherTransfer.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.0;
+
+contract EtherTransferTo {
+    function () public payable{
+        
+    }
+    
+    function getBalance() public returns (uint) {
+        return address(this).balance;
+    }
+}
+
+contract EtherTranferFrom {
+    
+}
